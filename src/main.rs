1a603bd230eba16f972ace8e0c819a8b585f19f4
AIGsniperYt <thahmimmustaq@gmail.com>
2026-08-30 13:51:55 +0100

    first working walker to find md files on our entire file system! :D

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,40 @@ fn find_mds(dir: &Path) {
     // naive idea would be to load the entire "list file" into RAM, just to read it
     // this is better because you get to read it at constant speeds, regardless how big the directory ("list file of children") is, by streaming (lazy & better!)
 
-    // if the raw_entries looks like an Ok(), pull it out into an "entries" variable (if it lets me assign this)
+    // 1. open the directory.
+    // if it fails (Err), return immediately (stop this function call), because we can't use this malformed dir anyways
+    // if it succeeds (Ok), bind the iterator to 'entries'.
     let Ok(entries) = fs::read_dir(dir) else { return }; // silently ignore errors, only filter the Ok() Results
 
     // this returns a ReadDir which is a file descriptor + cursor, we now have access to this directory
 
-    for entry in entries {
-    println!("{:?}", entry);  // entry is io::Result<DirEntry>
+    // 2. iterate over the directory
+    // "raw_entry" is a Result<DirEntry, Error> from the Os
+    for raw_entry in entries {
+
+        // 3. try to extract the raw DirEntry, if its Ok(), name the contents "entry"
+        // if its an err, skip this iteration and silently ignore 
+        if let Ok(entry) = raw_entry {
+
+            // if the program survives this far, we know that we have a clean DirEntry struct!
+            // we want its path for its filetype so lets get it, we can do anything to "path" and "entry" now!
+            let path = entry.path(); // the raw path looks like a normal literal string "/home" etc, see in debug mode by printing with {:?}
+            
+            if path.is_file() {
+                if let Some(ext) = path.extension() { // the ext is wrapped in a Some() because thats the shape the path.extension returns, Some(value) or None
+                    if ext == "md" {
+                        println!("{}", path.display()); // .display() strips the "" into clean human readable paths to look at! :)
+                    }
+                }
+            }
+
+            // 5. recursion, only if its a directory, call this function again on itself
+            if path.is_dir() {
+                find_mds(&path);
+            }
+
+        }
+
     }
 }
 
